f4e71e24286bb5990d20f7c0e4e3597dbd83e656
Exception-lviv <hlam.lviv.ua@gmail.com>
2018-04-05 02:38:33 +0300

    counter

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,13 +10,38 @@ import { ProductRepository } from '../model/product.repository';
 
 export class StoreComponent {
 
+  private defaultPage = 1;
+  public selectedCategory: string = null;
+  public productsPerPage = 4;
+  public selectedPage = this.defaultPage;
+
   constructor(private repository: ProductRepository) { }
 
   get products(): Product[] {
-    return this.repository.getProducts();
+    const pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository.getProducts(this.selectedCategory)
+        .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+    this.changePage(this.defaultPage);
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newPageSize: number) {
+    this.productsPerPage = Number(newPageSize);
+    this.changePage(this.defaultPage);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
   }
 }
